fac739e3ba20ccd47f60ede0779494992d30d8ad
programmingwithmalik <malikblassingale@gmail.com>
2023-03-13 18:07:24 -0400

    bruh

---
+++ src/test/java/com/syntheticdopamine/SynetheticDopamineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syntheticdopamine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynetheticDopamineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
